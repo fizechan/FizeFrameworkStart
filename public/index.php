f343d029447642b1a68d41a10dca792be1ed7a9f
Fize <chenfengzhan@qq.com>
2026-06-15 18:46:36 +0800

    feat：必要文件。

--- public/index.php
+++ public/index.php
@@ -2,12 +2,6 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Fize\Framework\App;
+
 $app = new App();
 $app->run();
-
-//$config = [
-//    'root_path' => dirname(dirname(__FILE__))
-//];
-//
-//$app = new App($config);
-//$app->run();
